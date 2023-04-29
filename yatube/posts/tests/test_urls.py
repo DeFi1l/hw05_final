95064effb2b5902c1067e6a549c1f77d97c8cf6c
Денис Филлер <denis.filler@yandex.ru>
2023-04-29 16:24:52 +0300

    +

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -84,12 +84,11 @@ class StaticURLTests(TestCase):
     def test_url_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
         templates_urls = {
-            '/': 'posts/index.html',
             f'/group/{self.group.slug}/': 'posts/group_list.html',
             f'/profile/{self.user.username}/': 'posts/profile.html',
             f'/posts/{self.post.id}/': 'posts/post_detail.html',
-            '/create/': 'posts/create_post.html',
-            f'/posts/{self.post.id}/edit/': 'posts/create_post.html',
+            '/create/': 'posts/post_create.html',
+            f'/posts/{self.post.id}/edit/': 'posts/post_create.html',
         }
         for url, template in templates_urls.items():
             with self.subTest(url=url):
